01c5d0b903bbee3d12f98950e7d6c049b02f86f6
Lakshya Shishir <checkinglakshya@gmail.com>
2024-06-12 12:56:44 +0530

    Initial Commit

---
+++ src/app/scheme/schemeCard.tsx
@@ -0,0 +1,9 @@
+export default function SchemeCard({ title, description }) {
+    return (
+      <div className="flex flex-col p-4 bg-white rounded shadow-md h-full">
+        <h2 className="text-xl font-bold mb-2">{title}</h2>
+        <p className="text-gray-700 flex-grow">{description}</p>
+        <button className="mt-4 bg-orange-500 text-white px-4 py-2 rounded">अधिक जानें</button>
+      </div>
+    );
+  }
